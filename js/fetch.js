7a14283a3dbfa5e3aaee73d90b97cd020db04ff5
facuandrean <facundoandrean22@gmail.com>
2023-09-30 14:38:41 -0300

    Progresando con fetch v.2

--- js/fetch.js
+++ js/fetch.js
@@ -10,7 +10,7 @@ export function fetchData() {
         Object.keys(json.comandos).forEach((comandoKey) => {
             const comando = json.comandos[comandoKey];
 
-            console.log(comando)
+            // console.log(comando)
             console.log(comando.banderas)
     
             const $card = d.createElement('div');
@@ -43,15 +43,55 @@ export function fetchData() {
             const $description = d.createElement('p');
             $description.innerHTML = `<b>Descripción:</b> ${comando.descripcion}`;
 
-            const $flags = d.createElement('ul');
-            $flags.innerHTML = `Banderas:`;
+            const $usingMode = d.createElement('p');
+            $usingMode.innerHTML = `<b>Modo de uso:</b> ${comando.modo_de_uso}`;
+            // const $usingExample = d.createElement('p');
+            // $usingExample.innerHTML = `<b>Ejemplo:</b> ${comando.ejemplo}`;
+            // const $usingResult = d.createElement('p');
+            // $usingResult.innerHTML = `<b>Resultado:</b> ${comando.resultado}`
 
-            // const $flag = d.createElement('li');
-            // $flag.innerHTML = `Nombre: ${comando.banderas}`;
+
+            const $flags = d.createElement('ul');
+            $flags.innerHTML = `<b>Banderas:</b>`;
+            
+            Object.keys(comando.banderas).forEach((banderaKey) => { 
+                const bandera = comando.banderas[banderaKey];
+                
+                const $flag = d.createElement('li');
+                $flag.innerHTML = `> <b><i>${banderaKey}</i></b> : ${bandera.descripcion || bandera}`;
+                
+                const $examples = d.createElement('ul');
+
+                // Verifica si la bandera tiene ejemplos
+                if (bandera.ejemplo) {
+                    const ejemplo = bandera.ejemplo;
+
+                    const $example = d.createElement('li');
+                    $example.innerHTML = `<u>Ejemplo de uso de la bandera</u>:`;
+                    $examples.appendChild($example);
+
+                    const $commandExample = d.createElement('li');
+                    $commandExample.innerHTML = `Comando: <b><i>${ejemplo.comando}</i></b>`;
+                    $examples.appendChild($commandExample);
+
+                    const $resultExample = d.createElement('li');
+                    $resultExample.innerHTML = `Resultado: ${ejemplo.resultado}`;
+                    $examples.appendChild($resultExample);
+                }
+
+                // Agrega el elemento de ejemplos al elemento de bandera
+                $flag.appendChild($examples);
+
+                $flags.appendChild($flag);
+            });
 
             $content.appendChild($command);
             $content.appendChild($name);
             $content.appendChild($description);
+            $content.appendChild($usingMode);
+            // $content.appendChild($usingExample);
+            // $content.appendChild($usingResult);
+            $content.appendChild($flags);
 
             $card.appendChild($title);
             $card.appendChild($content);
